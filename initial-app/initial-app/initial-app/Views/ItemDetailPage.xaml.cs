405f82721b0546f930201078021f7de64b344736
Ken <kenoh17@uw.edu>
2023-08-04 12:26:51 -0700

    added initial files

---
+++ initial-app/initial-app/initial-app/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using initial_app.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace initial_app.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
