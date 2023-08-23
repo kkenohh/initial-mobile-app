1d56d5f914ee622b603634bc53f0f28696a671a9
Ken <kenoh17@uw.edu>
2023-08-23 00:00:08 -0700

    ken started add todo page

---
+++ todo-list-app/todo-list-app/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace todo_list_app.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public int Priority { get; set; }
+        public string Description { get; set; }
+
+        
+        // Takes another Todo and compares the priority
+        // Returns - 0 for equal priority
+        //           a positive int if this Todo is higher priority
+        //           a negative int if this Todo is lower priority
+        /*public int HigherPriority(Todo other)
+        {
+            return this.Priority - other.Priority;
+        }*/
+    }
+}
